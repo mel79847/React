c6060922af32dbe3ae17c0e86b03a2190d8fc022
mel79847 <soncovillegasm@gmail.com>
2026-08-27 18:01:57 -0400

    Agregar workflow de Vite build

--- Clase 17/tres-en-raya/src/tresenraya.jsx
+++ Clase 17/tres-en-raya/src/tresenraya.jsx
@@ -1,5 +1,33 @@
 export default function TresEnRaya() {
   return (
-    <section> Hola </section>
+    <section>
+
+      <h2>Tres en Raya</h2>
+
+      <table style={{ borderCollapse: "collapse" }}>
+        <tbody>
+
+          <tr>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+          </tr>
+
+          <tr>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+          </tr>
+
+          <tr>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+            <td style={{ width: "60px", height: "60px", border: "1px solid black" }}></td>
+          </tr>
+
+        </tbody>
+      </table>
+
+    </section>
   );
 }
